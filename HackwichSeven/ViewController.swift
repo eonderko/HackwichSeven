d42f75f4d73c35b09679d50c99a733242a7a69bc
eonderko <eonderko@hawaii.edu>
2020-03-05 10:16:13 -1000

    completed parts 1-8

--- HackwichSeven/ViewController.swift
+++ HackwichSeven/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var displayLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.displayLabel.text = ""
+        self.view.backgroundColor = UIColor.purple
+        
+        
+        
+        
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func setLabelButtonPressed(_ sender: Any) {
+        let userInputText = textField.text
+        self.displayLabel.text = userInputText
+    }
+    
 }
 
